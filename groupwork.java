a586a4d30381cc3cd803d05e2b5cd57f5837ed34
kai1224 <52668158+kai1224@users.noreply.github.com>
2019-08-05 10:56:11 -0400

    Add files via upload

---
+++ groupwork.java
@@ -0,0 +1,31 @@
+package lastday;
+
+import java.util.Scanner;
+
+public class groupwork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Scanner java =  new Scanner(System.in);
+		
+		for (int i = 1; i < 100; i += 2) {
+			System.out.println(i);
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
